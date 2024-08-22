1959d243be04592e827fd49c3bcdd38084e787d3
mujtaba-kazmi15 <mujtabakazmi0321@gmail.com>
2024-08-22 14:08:52 +0500

    some new things is paste from another repository

---
+++ app.js
@@ -0,0 +1,210 @@
+// Q1:
+alert("Hi visitor, Welcome to Our Website")
+
+// Q2:
+alert("Error! Please Enter a valid password")
+
+// Q3:
+alert("Hey there! \nIt's Mujtaba's Website")
+
+// Q4:
+alert("I am a WEBSITE Developer")
+alert("I become a WEBSITE developer through Bano Qabil 3.0")
+alert("I making this JavaScript Project for the first time")
+
+// Q5:
+//( Done in project (ASSIGN/app.js) i had attached)
+
+// Q6:
+//( Done in project (ASSIGN/app.js) i had attached)
+
+// Q7:
+var userName = "mujtaba-kazmi15"
+
+// Q8:
+var myName = "Mujtaba Kamzi";
+
+// Q9:
+// (a).
+var message;
+
+// (b).
+message = "Hello World!";
+
+// (c).
+alert(message);
+
+// Q10:
+var namee = "Mujtaba Kazmi"
+var course = "WEBSITE Development - 1"
+var grade = "1st Year"
+var student_data = "name + course + grade"
+alert("Hello, my name is " + namee)
+alert("I am learning course of " + course)
+alert("I study in grade " + grade)
+
+// Q11:
+var email = "mujtabakazmi0321@gmail.com"
+alert("My email adress is; \n" + email)
+
+// Q12:
+console.log(email);
+
+// Q13:
+var emoji = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(emoji)
+console.log(emoji)
+
+// Q14:
+var age = "Age: 17"
+alert(age)
+
+// Q15:
+var visits = +prompt("Please! Tell us how many times you visited Our Website")
+alert("User visited this Website " + visits + " times")
+
+// Q16:
+var birthYear = "2007"
+console.log(birthYear)
+
+// Q17:
+alert("Welcome to www.AddidasClothing.com")
+var visName = prompt("What's Your Name?")
+var productTitle = prompt( visName + " what type of Product are you lookig for?")
+var quantity = prompt(" How many " + productTitle + " would you like to purchase?")
+console.log("" + visName + " ordered " + quantity + " " + productTitle + " on Addidas Clothing Store.")
+
+// Q18:
+var a = "Hey!" , b = "Whats Up!" , c = "Welcome!"
+
+// Q19:
+// LEGAL VARIABLE NAMES
+// (1).
+var name123
+
+// (2).
+var $last_name
+
+// (3).
+var _visiter
+
+// (4).
+var yourName
+
+// (5).
+var myname
+
+//ILLEGAL VARIABLE NAMES
+// // (1).
+// var 123
+
+// // (2).
+// var 2name
+
+// // (3).
+// var 2-names
+
+// // (4).
+// var class
+
+// // (5).
+// var your name
+
+// Q20:
+// (a).
+console.log("RULES FOR NAMING JAVASCRIPT VARIABLES")
+
+// (b).
+console.log("Variable names can only contain Letters, Digits(but a variable name cannot start with a digit), Underscores and Dollar Signs.")
+
+// (c).
+console.log("Variables must begin with a Letter, Underscore or Dollar Sign.")
+
+// (d).
+console.log("Variable names are case sensitive.")
+
+// (e).
+console.log("Variables should not be JS reserved keywords.")
+
+// Q21:
+var num1 = +prompt("Enter a number for addition;")
+var num2 = +prompt("Enter a number to add;")
+var total = console.log(num1 + num2)
+
+// Q22:
+// SUBTRACTION.
+var num3 = +prompt("Enter a number for subtraction;")
+var num4 = +prompt("Enter a number to subtract;")
+var total1 = console.log(num3 - num4)
+
+// MUTLIPLICATION.
+var num5 = +prompt("Enter a number for multiplication;")
+var num6 = +prompt("Enter a number to multiply;")
+var total2 = console.log(num5 * num6)
+
+// DIVISION.
+var num7 = +prompt("Enter a number for division;")
+var num8 = +prompt("Enter a number to divide;")
+var total3 = console.log(num7 / num8)
+
+// MODULUS.
+var num9 = +prompt("Enter a number for modulus;")
+var num10 = +prompt("Enter a number to modulo;")
+var total1 = console.log(num9 % num10)
+
+// Q23:
+// (a).
+var num
+
+// (b).
+console.log("Value after variable decleration is: " + num)
+
+// (c).
+num = 5 
+
+// (d).
+console.log("Intial value: " + num)
+
+// (e).
+num++
+
+// (f).
+console.log("Value after increment is: " + num)
+
+// (g).
+num = + 7
+
+// (h).
+console.log("Value after addition is: " + num)
+
+// (i).
+num--
+
+// (j).
+console.log("Value after decreament is: " + num)
+
+// (k).
+var remainder = num % 3
+
+// (l).
+console.log("After dividing the remainder is: " + remainder)
+
+// Q24:
+var cost = 600
+var total_cost = cost * 5
+console.log("The total cost after buying 5 tickets is " + total_cost + ".PKR")
+
+// Q25:
+var m2 = 5
+console.log("5 x 1 = " + m2*1)
+console.log("5 x 2 = " + m2*2)
+console.log("5 x 3 = " + m2*3)
+console.log("5 x 4 = " + m2*4)
+console.log("5 x 5 = " + m2*5)
+console.log("5 x 6 = " + m2*6)
+console.log("5 x 7 = " + m2*7)
+console.log("5 x 8 = " + m2*8)
+console.log("5 x 9 = " + m2*9)
+console.log("5 x 10 = " + m2*10)
+
+    //ASSINGMENT DONE//
