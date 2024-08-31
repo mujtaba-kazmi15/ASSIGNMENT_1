e6e0db6d57531860fa16721e2a7fa472b46ab4eb
mujtaba-kazmi15 <mujtabakazmi0321@gmail.com>
2024-08-31 23:44:46 +0500

    ok

---
+++ ASSIGNMENT_2/app.js
@@ -0,0 +1,10 @@
+alert("HEY VISITOR!")
+
+var palindromes = [
+    "Abba", "Aibohphobia", "Bib", "Bob", "Civic", "Deified", "Detartrated", "Dewed", 
+    "Eve", "Hannah", "Kayak", "Level", "Madam", "Malayalam", "Minim", "Mom", "Sis", 
+    "Noon", "Nun", "Otto", "Peep", "Pop", "Racecar", "Radar", "Redder", "Refer", "Gig",
+    "Repaper", "Rotator", "Rotavator", "Sagas", "Solos", "Stats", "Tattarrattat", "Dud",
+    "Tenet", "Aha", "Ava", "Dad", "Did", "Eke", "Ewe", "Eye", "Gag", "Gig", "Mum", "Wow",
+    "Pip", "Pup", "Reviver", "Sees", "Succus", "Deed", "Ama", "Ana", "Lil", "Tat", "Yay", "Pap", "Did", "Murdrum",
+]
