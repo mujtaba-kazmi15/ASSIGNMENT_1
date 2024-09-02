e849e4470e1aacdbe6f251c96a5dcef029c725b8
mujtaba-kazmi15 <mujtabakazmi0321@gmail.com>
2024-09-02 18:25:21 +0500

    done

--- ASSIGNMENT_2/app.js
+++ ASSIGNMENT_2/app.js
@@ -7,4 +7,18 @@ var palindromes = [
     "Repaper", "Rotator", "Rotavator", "Sagas", "Solos", "Stats", "Tattarrattat", "Dud",
     "Tenet", "Aha", "Ava", "Dad", "Did", "Eke", "Ewe", "Eye", "Gag", "Gig", "Mum", "Wow",
     "Pip", "Pup", "Reviver", "Sees", "Succus", "Deed", "Ama", "Ana", "Lil", "Tat", "Yay", "Pap", "Did", "Murdrum",
-]
+];
+
+var userInput = prompt("Please enter a word:");
+
+userInput = userInput.toLowerCase;
+
+var palindromesLowerCase = palindromes.map(function(palindromes) {
+    return palindromes.toLowerCase();
+});
+
+if (palindromesLowerCase.includes(userInput)) {
+    document.write("This is a palindrome");
+} else {
+    document.write("This is not a palindrome");
+}
